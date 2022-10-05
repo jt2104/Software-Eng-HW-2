564c53c186959bbc616a56e2f1926f417ba0f87b
Jackson Tran <jackson.tran@outlook.com>
2022-10-05 12:30:38 -0500

    Initial Commit

---
+++ Software Eng HW 2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Software Eng HW 2
+//
+//  Created by Jackson Tran on 10/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
